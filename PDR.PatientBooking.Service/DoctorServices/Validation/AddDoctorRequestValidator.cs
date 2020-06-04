37a8f879cf1c2852cd20d85847b5cdd5a70bd021
Developer256 <Seydi.T@gmail.com>
2020-06-04 11:58:06 +0100

    PDR Technical test

---
+++ PDR.PatientBooking.Service/DoctorServices/Validation/AddDoctorRequestValidator.cs
@@ -0,0 +1,85 @@
+﻿using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Service.DoctorServices.Requests;
+using PDR.PatientBooking.Service.Validation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PDR.PatientBooking.Service.DoctorServices.Validation
+{
+    public class AddDoctorRequestValidator : IAddDoctorRequestValidator
+    {
+        private readonly PatientBookingContext _context;
+
+        public AddDoctorRequestValidator(PatientBookingContext context)
+        {
+            _context = context;
+        }
+
+        public PdrValidationResult ValidateRequest(AddDoctorRequest request)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (MissingRequiredFields(request, ref result))
+                return result;
+
+            if (DoctorAlreadyInDb(request, ref result))
+                return result;
+
+            if (EmailAddressInvalid(request, ref result))
+                return result;
+
+            return result;
+        }
+
+        public bool MissingRequiredFields(AddDoctorRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+            if (string.IsNullOrEmpty(request.FirstName))
+                errors.Add("FirstName must be populated");
+
+            if (string.IsNullOrEmpty(request.LastName))
+                errors.Add("LastName must be populated");
+
+            if (string.IsNullOrEmpty(request.Email))
+                errors.Add("Email must be populated");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DoctorAlreadyInDb(AddDoctorRequest request, ref PdrValidationResult result)
+        {
+            if (_context.Doctor.Any(x => x.Email == request.Email))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A doctor with that email address already exists");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool EmailAddressInvalid(AddDoctorRequest request, ref PdrValidationResult result)
+        {
+            if (!Regex.IsMatch(request.Email,
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                RegexOptions.IgnoreCase, System.TimeSpan.FromMilliseconds(250)))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Email must be a valid email address");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ PDR.PatientBooking.Service/PatientServices/Validation/AddPatientRequestValidator.cs
@@ -0,0 +1,100 @@
+﻿using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Service.PatientServices.Requests;
+using PDR.PatientBooking.Service.Validation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PDR.PatientBooking.Service.PatientServices.Validation
+{
+    public class AddPatientRequestValidator : IAddPatientRequestValidator
+    {
+        private readonly PatientBookingContext _context;
+
+        public AddPatientRequestValidator(PatientBookingContext context)
+        {
+            _context = context;
+        }
+
+        public PdrValidationResult ValidateRequest(AddPatientRequest request)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (MissingRequiredFields(request, ref result))
+                return result;
+
+            if (PatientAlreadyInDb(request, ref result))
+                return result;
+
+            if (ClinicNotFound(request, ref result))
+                return result;
+
+            if (EmailAddressInvalid(request, ref result))
+                return result;
+
+            return result;
+        }
+
+        private bool MissingRequiredFields(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+            if (string.IsNullOrEmpty(request.FirstName))
+                errors.Add("FirstName must be populated");
+
+            if (string.IsNullOrEmpty(request.LastName))
+                errors.Add("LastName must be populated");
+
+            if (string.IsNullOrEmpty(request.Email))
+                errors.Add("Email must be populated");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool PatientAlreadyInDb(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            if (_context.Patient.Any(x => x.Email == request.Email))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A patient with that email address already exists");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ClinicNotFound(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            if (!_context.Clinic.Any(x => x.Id == request.ClinicId))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A clinic with that ID could not be found");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool EmailAddressInvalid(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            if (!Regex.IsMatch(request.Email,
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                RegexOptions.IgnoreCase, System.TimeSpan.FromMilliseconds(250)))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Email must be a valid email address");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
